3b0a3a8706a1600924997ee7990b415085b7244c
ZahidAlNahid <zahidalnahid@gmail.com>
2022-08-28 04:13:40 +0800

    Update
    Delete

--- Tutorials/Back Up/DJANGO_COURSE_1.xx/DJANGO_COURSE_1.xx/Django_Level_Two/first_project/first_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from first_app.models import Topic,Webpage,AccessRecord
-admin.site.register(Topic)
-admin.site.register(Webpage)
-admin.site.register(AccessRecord)
